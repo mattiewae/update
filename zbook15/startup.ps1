9e7519f7c509fe63718f20b3b55ba692ca056b73
mattiewae <Mathias.WAEGEMANS@VRT.BE>
2017-03-10 14:50:14 +0100

    faspex

--- zbook15/startup.ps1
+++ zbook15/startup.ps1
@@ -1,10 +1,20 @@
+Set-Location -Path $home\Downloads
+wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
+ 
+Expand-Archive .\faspex.zip 
+
+Move-Item .\faspex\faspex.website $home\Desktop 
+
+Remove-Item .\faspex.zip
+Remove-Item .\faspex
+Remove-Item "$home\Desktop\Aspera Faspex.website"
+
 $Connect = Test-Connection 'www.google.com' -Quiet
 
 if($Connect = $true){
-    choco install filezilla -y
     cup all -y 
 }
 else{
-    Start-Sleep -s 30
+    Start-Sleep -s 60
     cup all -y
 }
