25a26e9b66e2fece4614ae76821d78fc64f9c012
mattiewae <mathiaswaegemans@gmail.com>
2017-03-16 14:19:32 +0100

    Update startup.ps1

--- zbook15/startup.ps1
+++ zbook15/startup.ps1
@@ -1,13 +1,13 @@
 Set-Location -Path $home\Downloads
-wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
+wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip 
  
 Expand-Archive .\faspex.zip -force
 
-Move-Item .\faspex\faspex.website $home\Desktop -force
+Move-Item .\faspex\faspex.website $home\Desktop -force -recurse
 
-Remove-Item .\faspex.zip
-Remove-Item .\faspex
-Remove-Item "$home\Desktop\Aspera Faspex.website"
+Remove-Item .\faspex.zip -force
+Remove-Item .\faspex -force
+Remove-Item "$home\Desktop\Aspera Faspex.website" -force
 
 $Connect = Test-Connection 'www.google.com' -Quiet
 
