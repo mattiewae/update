0d745a3a0e5e87a988efc3832684deb9aa7e52fb
mattiewae <mathiaswaegemans@gmail.com>
2017-04-05 15:41:02 +0200

    Update startup.ps1

--- zbook15/startup.ps1
+++ zbook15/startup.ps1
@@ -1,21 +1,21 @@
 Set-Location -Path $home\Downloads
-wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
-wget https://github.com/mattiewae/update/blob/master/zbook15/Encoder.zip?raw=true -OutFile .\encoder.zip
+#wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
+#wget https://github.com/mattiewae/update/blob/master/zbook15/Encoder.zip?raw=true -OutFile .\encoder.zip
  
-Expand-Archive .\faspex.zip 
-Expand-Archive .\encoder.zip
+#Expand-Archive .\faspex.zip 
+#Expand-Archive .\encoder.zip
 
-Move-Item .\faspex\faspex.website $home\Desktop -force
-Move-Item .\encoder\encoder_V20.exe C:\encoder -force
-Move-Item .\encoder\encoder.lnk $home\Desktop -force
+#Move-Item .\faspex\faspex.website $home\Desktop -force
+#Move-Item .\encoder\encoder_V20.exe C:\encoder -force
+#Move-Item .\encoder\encoder.lnk $home\Desktop -force
 
-Remove-Item .\encoder
-Remove-Item .\encoder.zip
-Remove-Item .\faspex.zip
-Remove-Item .\faspex
-Remove-Item "$home\Desktop\Aspera Faspex.website"
-Remove-Item "$home\Desktop\Encoder.exe"
-Remove-Item "C:\encoder\Encoder_V19.exe"
+#Remove-Item .\encoder
+#Remove-Item .\encoder.zip
+#Remove-Item .\faspex.zip
+#Remove-Item .\faspex
+#Remove-Item "$home\Desktop\Aspera Faspex.website"
+#Remove-Item "$home\Desktop\Encoder.exe"
+#Remove-Item "C:\encoder\Encoder_V19.exe"
 
 $Connect = Test-Connection 'www.google.com' -Quiet
 
