9444324a955e390b606e783907a9d7631a0b891f
mattiewae <mathiaswaegemans@gmail.com>
2017-05-18 12:05:41 +0200

    Update startup.ps1

--- z420/startup.ps1
+++ z420/startup.ps1
@@ -1,13 +1,23 @@
-Set-Location -Path $home\Downloads
-wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
-Expand-Archive .\faspex.zip 
-Move-Item .\faspex\faspex.website $home\Desktop -Force
+#Set-Location -Path $home\Downloads
+#wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
+#Expand-Archive .\faspex.zip 
+#Move-Item .\faspex\faspex.website $home\Desktop -Force
 
+#Remove-Item .\faspex.zip -force
+#Remove-Item .\faspex -Recurse
+#Remove-Item "$home\Desktop\Aspera Faspex.website"
+#Remove-Item "$home\Desktop\Aspera Faspex.URL"
 
-Remove-Item .\faspex.zip -force
-Remove-Item .\faspex -Recurse
-Remove-Item "$home\Desktop\Aspera Faspex.website"
-Remove-Item "$home\Desktop\Aspera Faspex.URL"
+if($Connect = $true){
+    cup all -y 
+}
+else{
+    Start-Sleep -s 30
+    cup all -y
+}
+
+Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
+
 
 Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
 Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
@@ -20,10 +30,4 @@ Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
 
 $Connect = Test-Connection 'www.google.com' -Quiet
 
-if($Connect = $true){
-    cup all -y 
-}
-else{
-    Start-Sleep -s 30
-    cup all -y
-}
+
