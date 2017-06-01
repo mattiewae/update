485e16ac128d644b204421b4827225b0e57e7c86
mattiewae <mathiaswaegemans@gmail.com>
2017-06-01 11:03:04 +0200

    update

---
+++ z420Avid/startup.ps1
@@ -0,0 +1,37 @@
+#Set-Location -Path $home\Downloads
+#wget https://github.com/mattiewae/update/blob/master/z420/faspex.zip?raw=true -OutFile .\faspex.zip
+#Expand-Archive .\faspex.zip 
+#Move-Item .\faspex\faspex.website $home\Desktop -Force
+
+#Remove-Item .\faspex.zip -force
+#Remove-Item .\faspex -Recurse
+#Remove-Item "$home\Desktop\Aspera Faspex.website"
+#Remove-Item "$home\Desktop\Aspera Faspex.URL"
+
+$Connect = Test-Connection 'www.google.com' -Quiet
+if($Connect = $true){
+    cup all -y 
+}
+else{
+    Start-Sleep -s 30
+    cup all -y
+}
+
+
+Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
+Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
+Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
+Remove-Item "C:\Users\Public\Desktop\Libre*" -force
+Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
+Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
+Remove-Item "C:\Users\Public\Desktop\VLC*" -force
+
+
+#Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
+
+
+#choco install tightvnc -y
+
+
+
+
