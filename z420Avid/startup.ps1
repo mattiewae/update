e4b476cf4cf242c2f7eeacf2bf18c24feec07357
mattiewae <mathiaswaegemans@gmail.com>
2017-06-30 15:51:04 +0200

    Update startup.ps1

--- z420Avid/startup.ps1
+++ z420Avid/startup.ps1
@@ -8,14 +8,14 @@
 #Remove-Item "$home\Desktop\Aspera Faspex.website"
 #Remove-Item "$home\Desktop\Aspera Faspex.URL"
 
-$Connect = Test-Connection 'www.google.com' -Quiet
-if($Connect = $true){
-    cup all -y 
-}
-else{
-    Start-Sleep -s 30
-    cup all -y
-}
+#$Connect = Test-Connection 'www.google.com' -Quiet
+#if($Connect = $true){
+#    cup all -y 
+#}
+#else{
+#    Start-Sleep -s 30
+#    cup all -y
+#}
 
 
 Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
@@ -29,8 +29,7 @@ Remove-Item "C:\Users\ENG\Desktop\WinDir*" -force
 
 Clear-RecycleBin -Force
 
-
-#Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
+Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
 
 #choco install tightvnc -y
