81d60bfeef3dbc0e01873c7adde11bb792b5b9e1
mattiewae <mathiaswaegemans@gmail.com>
2017-08-30 11:02:33 +0200

    functions

--- z420Avid/startup.ps1
+++ z420Avid/startup.ps1
@@ -17,28 +17,69 @@
 #    cup all -y
 #}
 
+#choco install obs-studio -y
+#choco install win-youtube-dl -y
+#
+#Set-Location -Path $home\Downloads
+#wget https://github.com/mattiewae/update/raw/master/z420Avid/obs-studio.zip -OutFile .\obs.zip
+#Expand-Archive .\obs.zip 
+#Move-Item .\obs\obs-studio $env:APPDATA -Force 
+#
+#Remove-Item .\obs.zip -force
+#Remove-Item .\obs -Force -Recurse
 
-Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
-Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
-Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
-Remove-Item "C:\Users\Public\Desktop\Libre*" -force
-Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
-Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
-Remove-Item "C:\Users\Public\Desktop\VLC*" -force
-Remove-Item "C:\Users\ENG\Desktop\WinDir*" -force
+function OBS
+{
+  $choco = choco list -lo
+
+    if ($choco -like '*obs*') {
+    Write-Host 'obs installed'
+    }
+    else{
+    choco install -y obs-studio
+
+    Set-Location -Path $home\Downloads
+    wget https://github.com/mattiewae/update/raw/master/z420Avid/obs-studio.zip -OutFile .\obs.zip
+
+    Expand-Archive .\obs.zip 
+
+    Move-Item .\obs\obs-studio $env:APPDATA -Force 
+
+    Remove-Item .\obs.zip -force
+    Remove-Item .\obs -Force -Recurse
+    }
+}
+
+function Youtube-dl
+{
+  $choco = choco list -lo
+
+    if ($choco -like '*win-youtube-dl*') {
+    Write-Host 'win-youtube-dl installed'
+    }
+    else{
+    choco install -y win-youtube-dl
+    }
+}
+
+function RemoveItems
+{
+    Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
+    Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
+    Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
+    Remove-Item "C:\Users\Public\Desktop\Libre*" -force
+    Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
+    Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
+    Remove-Item "C:\Users\Public\Desktop\VLC*" -force
+    Remove-Item "C:\Users\ENG\Desktop\WinDir*" -force
+}
+
+OBS
+
+Youtube-dl
+
+RemoveItems
 
 Clear-RecycleBin -Force
 
 Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
-
-
-choco install obs-studio -y
-choco install win-youtube-dl -y
-
-Set-Location -Path $home\Downloads
-wget https://github.com/mattiewae/update/raw/master/z420Avid/obs-studio.zip -OutFile .\obs.zip
-Expand-Archive .\obs.zip 
-Move-Item .\obs\obs-studio $env:APPDATA -Force 
-
-Remove-Item .\obs.zip -force
-Remove-Item .\obs -Force -Recurse
