457a0f0b94c47d009e20a4b3a7333c4838b99e80
mattiewae <mathiaswaegemans@gmail.com>
2017-11-21 17:41:00 +0100

    Update startup.ps1

--- z420/startup.ps1
+++ z420/startup.ps1
@@ -28,6 +28,7 @@ Remove-Item "C:\Users\Public\Desktop\Libre*" -force
 Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
 Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
 Remove-Item "C:\Users\Public\Desktop\VLC*" -force
+Remove-Item "C:\Users\Public\Desktop\OBS*" -force
 
 
 Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
@@ -45,7 +46,7 @@ function Wait-KeyPress($prompt='Druk op een toets ...!') {
 }
 
 
-&("C:\Users\ENG\Desktop\Admin Tools\Scripts\ImageChecker.ps1")
+#&("C:\Users\ENG\Desktop\Admin Tools\Scripts\ImageChecker.ps1")
 
 #Wait-KeyPress
 
