ce6e4ba11120b7b1a748c5a33a9c3a639bfd1c17
mattiewae <mathiaswaegemans@gmail.com>
2018-02-06 17:26:15 +0100

    Update startup.ps1

--- z420/startup.ps1
+++ z420/startup.ps1
@@ -61,6 +61,7 @@ Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
 Remove-Item "C:\Users\Public\Desktop\VLC*" -force
 Remove-Item "C:\Users\Public\Desktop\OBS*" -force
 Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
+Remove-Item "C:\Users\Public\Desktop\Firefox.lnk"
 
 Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
