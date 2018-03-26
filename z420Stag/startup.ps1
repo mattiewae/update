70f56dfe2be4b779710541547b1300cdc1abe944
mattiewae <mathiaswaegemans@gmail.com>
2018-03-26 17:19:39 +0200

    update

--- z420Stag/startup.ps1
+++ z420Stag/startup.ps1
@@ -16,10 +16,12 @@ Remove-Item C:\Users\ENG\Desktop\encoder_V2*
 
 $encoder = Test-Path C:\encoder\encoder_V33_16CH.exe
 
-if($encoder -eq $true){
+function UpdateEncoder{
+    if($encoder -eq $true){
  
-    # encoder uptodate
-}
+    Write-Host "encoder uptodate"
+
+    }
 else{
     Set-Location C:\Users\ENG\Downloads
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -29,6 +31,7 @@ else{
 	Copy-Item Encoder_V33_16ch.exe 'C:\encoder\'
 	Remove-Item Encoder_V33_16ch.exe
 	Remove-Item C:\Users\ENG\Desktop\encoderV32*.exe 
+}
 }
 
 function Presets{
@@ -51,51 +54,58 @@ function Presets{
             }
 }
 
+function UpdateApps{
+    $Connect = Test-Connection 'www.google.com' -Quiet
+         if($Connect -eq $true){
+         cinst wget -y
+         cinst flashplayerplugin -y
+         cinst flashplayeractivex -y
+         cinst googlechromex64 -y 
+         cinst firefox -y 
+         cinst notepadplusplus.install -y
+         cinst jre8 -y 
+         cinst vlc -y
+         cinst ccleaner -y
+         cinst adobeair -y
+         cinst windirstat -y
+         cinst quicktime -y 
+         cinst irfanview -y
+         cinst 7zip.install -y
+         cinst javaruntime -y
+         cinst putty -y  
+         cinst libreoffice -y
+         cup all -y 
+        }
+    else{
+        Start-Sleep -s 30
+        cup all -y
+   }
+}
+
+function Remove-DesktopItems{
+    Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
+    Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
+    Remove-Item "C:\Users\ENG\Desktop\Mozilla*" -force
+    Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
+    Remove-Item "C:\Users\Public\Desktop\Libre*" -force
+    Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
+    Remove-Item "C:\Users\Public\Desktop\Google*" -force
+    Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
+    Remove-Item "C:\Users\Public\Desktop\VLC*" -force
+    Remove-Item "C:\Users\Public\Desktop\OBS*" -force
+    Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
+    Remove-Item "C:\Users\Public\Desktop\Firefox*"
+    Remove-Item "C:\Users\Public\Desktop\Google*"
+}
+
+UpdateEncoder
+
+UpdateApps
+
 Presets
 
-$Connect = Test-Connection 'www.google.com' -Quiet
-if($Connect -eq $true){
-    cinst wget -y
-    cinst flashplayerplugin -y
-    cinst flashplayeractivex -y
-    cinst googlechromex64 -y 
-    cinst firefox -y 
-    cinst notepadplusplus.install -y
-    cinst jre8 -y 
-    cinst vlc -y
-    cinst ccleaner -y
-    cinst adobeair -y
-    cinst windirstat -y
-    cinst quicktime -y 
-    cinst irfanview -y
-    cinst 7zip.install -y
-    cinst javaruntime -y
-    cinst putty -y  
-    cinst libreoffice -y
-    cup all -y 
-}
-else{
-    Start-Sleep -s 30
-    cup all -y
-}
-
-
-Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
-Remove-Item "C:\Users\Public\Desktop\Avidemux*" -force
-Remove-Item "C:\Users\ENG\Desktop\Mozilla*" -force
-Remove-Item "C:\Users\ENG\Desktop\Boxstarter*" -force
-Remove-Item "C:\Users\Public\Desktop\Libre*" -force
-Remove-Item "C:\Users\Public\Desktop\Chrome*" -force
-Remove-Item "C:\Users\Public\Desktop\Google*" -force
-Remove-Item "C:\Users\Public\Desktop\CCleaner*" -force
-Remove-Item "C:\Users\Public\Desktop\VLC*" -force
-Remove-Item "C:\Users\Public\Desktop\OBS*" -force
-Remove-Item "C:\Users\Public\Desktop\Mozilla*" -force
-Remove-Item "C:\Users\Public\Desktop\Firefox*"
-Remove-Item "C:\Users\Public\Desktop\Google*"
+Remove-DesktopItems
 
 Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
 Log-Message "Update Completed" | Out-File -Append "C:\Users\ENG\Desktop\Admin Tools\UpdateLog.txt"
-
-
