d68545df1771d5922d66363f59105832249f00ea
mattiewae <mathiaswaegemans@gmail.com>
2018-04-25 14:03:25 +0200

    Update update.ps1

--- z420/update.ps1
+++ z420/update.ps1
@@ -1,4 +1,8 @@
-﻿Set-ExecutionPolicy RemoteSigned
+#Set-ExecutionPolicy RemoteSigned
 
-Write-Host "Update Startup"
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/update/master/z420/startup.ps1'))
+#Write-Host "Update Startup"
+#iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/update/master/zbook15/startup.ps1'))
+
+Set-Location -Path $env:TEMP 
+
+wget https://raw.githubusercontent.com/mattiewae/update/master/z420/startup.ps1 -OutFile update.ps1
