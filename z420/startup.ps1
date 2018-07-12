af300596c378a967d95f00b6a199239b1c08b01a
mattiewae <mathiaswaegemans@gmail.com>
2018-07-12 11:29:15 +0200

    Update startup.ps1

--- z420/startup.ps1
+++ z420/startup.ps1
@@ -343,7 +343,13 @@ function ClearAdobeMediaCache{
         # Delete files older than the $limit.
 		Get-ChildItem -Path $AdobeImport -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
 		# Delete any empty directories left behind after deleting the old files.
-		Get-ChildItem -Path $AdobeImport -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+		Get-ChildItem -Path $AdobeImport -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse	
+		
+    $AdobeExport = "E:\AdobeExport"
+		# Delete files older than the $limit.
+		Get-ChildItem -Path $AdobeExport -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+		# Delete any empty directories left behind after deleting the old files.
+		Get-ChildItem -Path $AdobeExport -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null }			
  }
 	
 	OT
