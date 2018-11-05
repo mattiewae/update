a45d08e871544ebb6b46c102291ec6604374d5ec
mattiewae <mathiaswaegemans@gmail.com>
2018-11-05 10:57:26 +0100

    Update startup.ps1

--- z420/startup.ps1
+++ z420/startup.ps1
@@ -356,6 +356,12 @@ function ClearAdobeMediaCache{
 		Get-ChildItem -Path $AdobeExport -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
 		# Delete any empty directories left behind after deleting the old files.
 		Get-ChildItem -Path $AdobeExport -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null }			
+   
+   $AdobeImport = "E:\AdobeProjects"
+        # Delete files older than the $limit.
+		Get-ChildItem -Path $AdobeImport -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+		# Delete any empty directories left behind after deleting the old files.
+		Get-ChildItem -Path $AdobeImport -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
  }
 	
 	OT
